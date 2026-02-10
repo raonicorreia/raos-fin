bc55ebdb3c02f30fada57a837af3b1d74c14a574
Raoni Correia da Costa <raonicorreia@gmail.com>
2026-02-09 23:48:54 -0300

    Implementando segurança

--- backend/src/main/java/com/raos/fin/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/raos/fin/security/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,9 +27,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, 
-                                  HttpServletResponse response, 
-                                  FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, 
+                                  @NonNull HttpServletResponse response, 
+                                  @NonNull FilterChain filterChain) throws ServletException, IOException {
         
         final String authorizationHeader = request.getHeader("Authorization");
 
